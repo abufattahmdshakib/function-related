c782f7d0dbcf8edc7f0a9799adac5a749fdb7edf
abufatttahmdshakib <abufattahmdshakib21@gmail.com>
2025-02-13 04:24:07 +0600

    function syntax

--- basic-syntax.js
+++ basic-syntax.js
@@ -12,12 +12,27 @@
 // const total = sum ( 'shakib', ' , ' ,'labonno');
 // console.log( total);
 
-function macherTorkari(mach, shobji, mosla) {
-    console.log('tel gorom korun');
-    console.log('mosla furan');
-    console.log('shobji den');
-    console.log('mach den');
-    return 'susadhu macher torkari prostut';
+// function macherTorkari(mach, shobji, mosla) {
+//     console.log('tel gorom korun');
+//     console.log('mosla furan');
+//     console.log('shobji den');
+//     console.log('mach den');
+//     return 'susadhu macher torkari prostut';
+// }
+// const results = macherTorkari('a' , 'b' ,'c');
+// console.log( results);
+
+function number(a, b, c, d, e) {
+    const total = a + b + c + d + e ;
+    console.log(total);
+    const gun = total * a;
+    console.log(gun);
+    const vag = total / gun;
+    if ( vag < 50 ){
+        console.log('temon kichu na ektu paknami');
+    }
+    else {
+        console.log('temon kichu na betha paichi');
+    }
 }
-const results = macherTorkari('a' , 'b' ,'c');
-console.log( results);
+number(8, 5, 6, 4, 7)
