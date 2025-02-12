8608b9db6f8fb94c71016f780dac317f4fb4ad38
abufatttahmdshakib <abufattahmdshakib21@gmail.com>
2025-02-13 03:01:09 +0600

    function syntax

--- basic-syntax.js
+++ basic-syntax.js
@@ -1,6 +1,12 @@
-function dhaka() {
-    console.log('ami vat khabo');
-    console.log('ami vat khai na');
-    console.log('tumi kothai jaw')
+// function dhaka() {
+//     console.log('ami vat khabo');
+//     console.log('ami vat khai na');
+//     console.log('tumi kothai jaw')
+// }
+// dhaka();
+function sum (x , y ,z) {
+    const results = x + y + z ;
+    return results ; 
 }
-dhaka();
+const total = sum ( 'shakib', ' , ' ,'labonno');
+console.log( total);
