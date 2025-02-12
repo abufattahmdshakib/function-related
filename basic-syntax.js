759fde42485f0c346e03fb19ab8992fd479e6d96
abufatttahmdshakib <abufattahmdshakib21@gmail.com>
2025-02-13 03:15:57 +0600

    function syntax

--- basic-syntax.js
+++ basic-syntax.js
@@ -4,9 +4,20 @@
 //     console.log('tumi kothai jaw')
 // }
 // dhaka();
-function sum (x , y ,z) {
-    const results = x + y + z ;
-    return results ; 
+
+// function sum (x , y ,z) {
+//     const results = x + y + z ;
+//     return results ; 
+// }
+// const total = sum ( 'shakib', ' , ' ,'labonno');
+// console.log( total);
+
+function macherTorkari(mach, shobji, mosla) {
+    console.log('tel gorom korun');
+    console.log('mosla furan');
+    console.log('shobji den');
+    console.log('mach den');
+    return 'susadhu macher torkari prostut';
 }
-const total = sum ( 'shakib', ' , ' ,'labonno');
-console.log( total);
+const results = macherTorkari('a' , 'b' ,'c');
+console.log( results);
