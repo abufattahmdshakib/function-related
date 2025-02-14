7ef5caa536d72936271292fb45149b8785c670e6
abufatttahmdshakib <abufattahmdshakib21@gmail.com>
2025-02-15 02:49:57 +0600

    odds number is done

--- basic-syntax.js
+++ basic-syntax.js
@@ -83,30 +83,42 @@
 
 
 
-function oddNumbers(numbers) {
-    // console.log(numbers);
-    const odds = [];
-    for( const number of numbers){
-        if( number % 2 === 1){
-            console.log('odds numbers:', number);
-            odds.push(number);
+// function oddNumbers(numbers) {
+//     // console.log(numbers);
+//     const odds = [];
+//     for( const number of numbers){
+//         if( number % 2 === 1){
+//             console.log('odds numbers:', number);
+//             odds.push(number);
+//         }
+//     }
+//     let sum = 0;
+//     for( const number of odds){
+//         sum = sum + number;
+//     }
+//     const count  = odds.length;
+//     console.log('odds size:', count)
+//     console.log('sum all oddNumbers:', sum);
+//     console.log('gor odds:', sum / count);
+
+// }
+
+// const numbers = [71, 23, 22, 25, 65, 54, 67];
+//  const oddsNumbers = oddNumbers(numbers);
+
+const birayniKhors = ['taijul', 'shakib', 'taj', 'taijul', 'raj', 'shakib', 'topu', 'milon', 'shakib', 'raj', 'milon', 'topu', 'fahim'];
+function noDuplicate(array) {
+    const singel = [];
+    for (const nam of birayniKhors) {
+        if (singel.includes(nam) === false) {
+            singel.push(nam);
         }
     }
-    let sum = 0;
-    for( const number of odds){
-        sum = sum + number;
-    }
-    const count  = odds.length;
-    console.log('odds size:', count)
-    console.log('sum all oddNumbers:', sum);
-    console.log('gor odds:', sum / count);
-
+return singel;
 }
 
-const numbers = [71, 23, 22, 25, 65, 54, 67];
-oddNumbers(numbers);
-
-
+const singlearray = noDuplicate(birayniKhors);
+console.log('singel persone is :', singlearray);
 
 
 
